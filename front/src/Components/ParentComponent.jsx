4a2e1f0fc269e34c78b10da3b31f2a0ce2327dd4
manicdon7 <manicdon7@gmail.com>
2024-02-19 15:37:36 +0530

    nav

--- front/src/Components/ParentComponent.jsx
+++ front/src/Components/ParentComponent.jsx
@@ -1,41 +1,75 @@
-// ParentComponent.js
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Connect from '../pages/Connect';
 
-function ParentComponent() {
-  const [userEmail, setUserEmail] = useState(null);
-
-  return (
-    <div>
-<div className="py-2 px-20 bg-black w-full">
-  <div className="flex justify-between items-center text-white py-2 bg-white/30 rounded-full px-4 poppins-font">
-    <div className="flex items-center">
-      <h1 className="text-yellow-400 text-2xl font-bold">🎟️EasyPass</h1>
-    </div>
-    <div className="flex items-center gap-6">
-      <a href="/" className="text-lg hover:underline">
-        Home
-      </a>
-      <a href="/Dashboard" className="text-lg hover:underline">
-        Dashboard
-      </a>
-      <a href="/host" className="text-lg hover:underline">
-        Host
-      </a>
-      <a href="/events" className="text-lg hover:underline">
-        Events
-      </a>
-    </div>
-    <div className="flex items-center gap-6">
-      {userEmail && (
-        <span className="text-lg">{userEmail}</span>
-      )}
-      <Connect setUserEmail={setUserEmail} />
-    </div>
-  </div>
-</div>
-    </div>
-  );
+function NavLink({to, children}) {
+    return <a href={to} className={`mx-4`}>
+        {children}
+    </a>
 }
 
-export default ParentComponent;
+function MobileNav({open, setOpen}) {
+    return (
+        <div className={`absolute top-0 left-0 h-screen w-screen font-Arvo bg-gray-900 transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+            <div className="flex items-center justify-center filter drop-shadow-md bg-gray-900 h-20">
+                <a className="text-xl font-semibold text-white" href="/">🎟️EasyPass</a>
+            </div>
+            <div className="flex flex-col ml-4">
+            <a className="text-xl font-normal my-4 text-white" href="/Dashboard" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Dashboard
+                </a>
+                <a className="text-xl font-normal my-4 text-white" href="/Dashboard" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Dashboard
+                </a>
+                <a className="text-xl font-normal my-4 text-white" href="/about" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    About
+                </a>
+                <a className="text-xl font-normal my-4 text-white" href="/contact" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Contact
+                </a>
+            </div>  
+        </div>
+    )
+}
+
+export default function ParentComponent() {
+  const [userEmail, setUserEmail] = useState(null);
+    const [open, setOpen] = useState(false)
+    return (
+        <nav className="flex justify-center filter drop-shadow-md font-Arvo bg-gray-900 px-4 py-4 h-20 items-center text-white">
+            <MobileNav open={open} setOpen={setOpen}/>
+            <div className="w-3/12 flex items-center">
+                <a className="text-2xl font-semibold" href="/">🎟️EasyPass</a>
+            </div>
+            <div className="w-9/12 flex justify-center items-center">
+
+                <div className="z-50 flex relative w-8 h-8 flex-col justify-center items-center md:hidden" onClick={() => {
+                    setOpen(!open)
+                }}>
+                    {/* hamburger button */}
+                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+                    <span className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+                </div>
+
+                <div className="hidden md:flex justify-center">
+                <NavLink to="/Dashboard">
+                        HOME
+                    </NavLink>
+                    <NavLink to="/Dashboard">
+                        DASHBOARD
+                    </NavLink>
+                    <NavLink to="/contact">
+                        HOST
+                    </NavLink>
+                    <NavLink to="/about">
+                        EVENTS
+                    </NavLink>
+                </div>
+            </div>
+            {userEmail && (
+            <span className="text-sm md:text-lg">{userEmail}</span>
+          )}
+          <Connect setUserEmail={setUserEmail} />
+        </nav>
+    )
+}
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -8,10 +8,10 @@ const bg = {
 const Home = () => {
     return (
         <div className="h-screen play-font" style={bg}> 
-            <main className="grid grid-cols-2">
-                <div className="text-white font-semibold  mt-36 ml-10 poppins-font"> 
-                    <h1 className="text-7xl" > EasyPass: <span className="bg-gradient-to-r from-red-800 via-yellow-600 to-yellow-500 text-transparent bg-clip-text"> Festive Passes, Your Way </span></h1>
-                    <p className="text-lg  mt-8 mr-28">Experience seamless event hosting and ticket booking with EasyPass  Embrace the future of ticketing - Your hassle-free ticketing solution!</p>
+            <main className="grid md:grid-cols-2">
+                <div className="text-white font-semibold mt-10 md:mt-36 ml-10 poppins-font"> 
+                    <h1 className="text-2xl md:text-7xl"> EasyPass: <span className="bg-gradient-to-r from-red-800 via-yellow-600 to-yellow-500 text-transparent bg-clip-text">Festive Passes, Your Way </span></h1>
+                    <p className="text-xl md:text-lg  mt-8 mx-2 md:mr-28">Experience seamless event hosting and ticket booking with EasyPass  Embrace the future of ticketing - Your hassle-free ticketing solution!</p>
                     <a href="/events">
                     <button className="bg-white text-black mt-10 font-bold text-xl px-3 border-2 py-2 rounded-full hover:bg-black hover:border-2 hover:border-yellow-400 hover:text-yellow-400">Book Now</button>
                     </a>
