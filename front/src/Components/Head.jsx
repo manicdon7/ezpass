25c3394464d9dfad63de622d3159cfb2ea57c069
manicdon7 <manicdon7@gmail.com>
2024-05-12 17:38:17 +0530

    url

--- front/src/Components/Head.jsx
+++ front/src/Components/Head.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
+import home from '../assets/home.png';
 
-const Head = ({ title, description, image }) => {
-  const [previewImage, setPreviewImage] = useState(image);
-
-  // Function to handle image load errors
-  const handleImageError = () => {
-    // Set a default preview image if the provided image fails to load
-    setPreviewImage('https://github.com/manicdon7/ezpass/blob/master/front/src/assets/home.png');
-  };
-
+const Head = ({ title, description, image, imageAlt }) => {
   return (
     <Helmet>
       <title>{title}</title>
       <meta property="og:title" content={title} />
+      <meta property='description' content={description}/>
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={previewImage} onError={handleImageError} />
+      <meta property="og:image" content={image} />
+      <meta property='or:url' content={window.location.pathname + window.location.search}/>
+      <meta name='twitter:card' content='summary_large_image'/>
+      <meta property='og:url' content={home}/>
+      <meta name='twitter:image:alt' content={imageAlt}/>
     </Helmet>
   );
 };
