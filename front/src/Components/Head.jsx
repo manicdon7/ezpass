9fb9d308b055dfb21fa605bcf450dde313ec7661
manicdon7 <manicdon7@gmail.com>
2024-05-12 16:36:12 +0530

    image

--- front/src/Components/Head.jsx
+++ front/src/Components/Head.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 
 const Head = ({ title, description, image }) => {
+  const [previewImage, setPreviewImage] = useState(image);
+
+  // Function to handle image load errors
+  const handleImageError = () => {
+    // Set a default preview image if the provided image fails to load
+    setPreviewImage('https://github.com/manicdon7/ezpass/blob/master/front/src/assets/home.png');
+  };
+
   return (
     <Helmet>
       <title>{title}</title>
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={previewImage} onError={handleImageError} />
     </Helmet>
   );
 };
